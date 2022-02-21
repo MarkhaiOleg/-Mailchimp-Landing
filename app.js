6be9e052462b72b480875eb226a668e556bba9cf
Oleg Markhai <oleg987663@gmail.com>
2022-02-21 20:23:51 +0200

    delete card

---
+++ app.js
@@ -0,0 +1,23 @@
+var card= document.querySelectorAll(".section-2-card");
+var buttonDelete = document.querySelectorAll('.delete');
+
+
+buttonDelete[0].addEventListener('click', () =>{
+    card[0].style.display = "none";
+})
+
+buttonDelete[1].addEventListener('click', () =>{
+    card[1].style.display = "none";
+})
+
+buttonDelete[2].addEventListener('click', () =>{
+    card[2].style.display = "none";
+})
+
+buttonDelete[3].addEventListener('click', () =>{
+    card[3].style.display = "none";
+})
+
+
+
+
